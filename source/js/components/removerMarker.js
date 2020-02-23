f045fecd28f35961c1b85c491d468b95e2fbc800
Albert <sab4856@gmail.com>
2020-02-23 18:59:14 +0800

    Update 2020-02-23T10:59:09.525Z

--- source/js/components/removerMarker.js
+++
@@ -1,7 +0,0 @@
-function removerMarker() {
-  _map.eachLayer(function (layer) {
-    if (layer instanceof L.Marker) {
-      _map.removeLayer(layer)
-    }
-  });
-}
--- source/js/components/upDataCounty.js
+++
@@ -1,9 +0,0 @@
-function upDataCounty(areaData) {
-  let str = '';
-  for (let i = 0; i < areaData.length; i++) {
-    str += `
-		<option value="${areaData[i].CityName}">${areaData[i].CityName}</option>
-		`
-  }
-  county.innerHTML = str;
-};
