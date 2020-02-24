259d25d472b0361883d876df1c42aef30d272090
Albert <sab4856@gmail.com>
2020-02-24 20:11:22 +0800

    add map attribution

--- source/js/components/buildMap.js
+++ source/js/components/buildMap.js
@@ -3,7 +3,9 @@ function buildMap() {
     center: [25.04828, 121.51435],
     zoom: 16,
   });
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '作者 Albert || 設計師 Wendy ',
+  }
   ).addTo(_map)
   // 使用 control.locate 套件
   L.control.locate({
