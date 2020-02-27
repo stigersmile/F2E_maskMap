6005ab76244eac815fcde6b0e1ad05fdd5c19c6a
Albert <sab4856@gmail.com>
2020-02-27 22:18:24 +0800

    空白刪除

--- source/js/components/buildMap.js
+++ source/js/components/buildMap.js
@@ -1,15 +1,16 @@
 const buildMap = () => {
   map = L.map('mapId', {
     center: [25.04828, 121.51435],
-    zoom: 16,
+    zoom: 16
   });
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '作者 Albert || 設計師 Wendy ',
-
-  }
-  ).addTo(map)
+    attribution: '作者 Albert || 設計師 Wendy '
+  }).addTo(map);
   // 使用 control.locate 套件
-  L.control.locate({
-    showPopup: false
-  }).addTo(map).start();
-}
+  L.control
+    .locate({
+      showPopup: false
+    })
+    .addTo(map)
+    .start();
+};
